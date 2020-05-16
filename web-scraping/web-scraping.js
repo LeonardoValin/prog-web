63533bb617ef083c05c341038172fc53d7cea068
Leonardo Henrique Alves Valin <51098879+LeonardoValin@users.noreply.github.com>
2020-05-15 23:03:20 -0300

    Add files via upload

---
+++ web-scraping/web-scraping.js
@@ -0,0 +1,8 @@
+let arrayJson = [];
+$.each($(".product-card"), function() {
+titulo = $(this).find(".product-card__title").text();
+desconto = $(this).find(".promotion-tag-discount").text();
+valor = $(this).find(".promotion-price").text();
+arrayJson.push({titulo,desconto,valor});
+});
+console.log(JSON.stringify(arrayJson, null, 2));
